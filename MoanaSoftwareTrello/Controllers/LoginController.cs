d9f636e2da608820fa4c0dabd858ee071daeec22
FeerLaas <csardas.f.l@icloud.com>
2022-09-07 11:41:13 +0200

    Create User Login/Register View fix images and css order

--- MoanaSoftwareTrello/Controllers/LoginController.cs
+++ MoanaSoftwareTrello/Controllers/LoginController.cs
@@ -5,10 +5,28 @@ namespace MoanaSoftwareTrello.Controllers
 {
     public class LoginController : Controller
     {
+        static HttpClient client = new HttpClient();
+        [HttpGet]
         public IActionResult Index(bool logged = false)
         {
-            User userModel = new User();
+            if (logged) RedirectToAction("Index","Home");
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User userModel)
+        {
+            // login ...
+            // login is valid add token to local
+            bool success = true;
+            if (success) return RedirectToAction("Index", "Home");
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register(bool logged = false)
+        {
+            if (logged) RedirectToAction("Index", "Home");
+            User userModel = new User();
+            return View(userModel);
         }
     }
 }
--- MoanaSoftwareTrello/Models/UserModel.cs
+++ MoanaSoftwareTrello/Models/UserModel.cs
@@ -2,7 +2,10 @@
 {
     public class User
     {
-       public string Email { get; set; }
-       public string Password { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? userId { get; set; }
+        public string? message { get; set; }
+
     }
 }
