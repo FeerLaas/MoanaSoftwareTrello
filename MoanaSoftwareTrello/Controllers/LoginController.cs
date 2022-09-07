4ca713c0302f2b2824a1472439a8944b58ff170b
FeerLaas <csardas.f.l@icloud.com>
2022-09-07 10:16:29 +0200

    Add project files.

---
+++ MoanaSoftwareTrello/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoanaSoftwareTrello.Models;
+
+namespace MoanaSoftwareTrello.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index(bool logged = false)
+        {
+            User userModel = new User();
+            return View();
+        }
+    }
+}
---
+++ MoanaSoftwareTrello/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoanaSoftwareTrello.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
