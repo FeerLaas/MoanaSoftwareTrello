3ed871477b51f7af3e7c89336533e3816ab88ab1
FeerLaas <csardas.f.l@icloud.com>
2022-09-07 23:37:31 +0200

    fix css and layout

--- MoanaSoftwareTrello/Program.cs
+++ MoanaSoftwareTrello/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 //builder.Services.AddHostedService<ApiService>();
 builder.Services.AddSingleton<ApiService>();
-
+builder.Services.AddSignalR();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromHours(4);
